8478d8fca0a7c6b1bf4d39a1235ef717429ef878
Stan <taix97@outlook.com>
2024-08-17 16:12:46 +0300

    char controller

--- GMTK/Assets/Scripts/PlayerController.cs
+++ GMTK/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float mouseSensitivity;
 
     private Rigidbody rb;
+    private Camera cam;
+
+    private float cameraYrotation = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
     }
 
     void Update()
     {
         CharacterMovement();
+        CharacterRotation();
     }
 
     void CharacterMovement()
@@ -21,12 +27,31 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        float mass = rb.mass;
-        float baseSpeed = speed;
-        float calculatedSpeed = baseSpeed / mass;
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
 
-        Vector3 movement = new Vector3(moveX, 0, moveZ).normalized * calculatedSpeed;
+        forward.y = 0f;
+        right.y = 0f;
+
+        Vector3 desiredMoveDirection = (forward * moveZ + right * moveX).normalized;
+
+        Vector3 movement = desiredMoveDirection * speed;
+
         movement.y = rb.velocity.y;
+
         rb.velocity = movement;
+    }
+
+    void CharacterRotation() { 
+        float xRot = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float yRot = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+
+        cameraYrotation -= yRot;
+       
+        cameraYrotation = Mathf.Clamp(cameraYrotation, -90f, 90f);
+
+        cam.transform.localEulerAngles = Vector3.right * cameraYrotation;
+        transform.Rotate(Vector3.up * xRot);
     }
 }
